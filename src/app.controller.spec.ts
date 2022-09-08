0f938f789ed57b5c7404d883535367da6b19f94f
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2022-09-08 20:08:28 +0300

    hello taltech

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello TalTech', () => {
+      const greetings = "Hello TalTech";
+      expect(appController.getHello()).toEqual({greetings});
     });
   });
 });
---
+++ src/dto/GreetingsDto.ts
@@ -0,0 +1,3 @@
+interface GreetingsDto {
+  greetings: string
+}
